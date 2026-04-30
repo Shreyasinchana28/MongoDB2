22de483b512bb8e1012dbe9fa35770930ba5e1a1
Shreyasinchana28 <shreyasinchanar@gmail.com>
2026-04-30 11:59:43 +0530

    Create Program28.js

---
+++ Program28.js
@@ -0,0 +1,85 @@
+db.bookings.insertMany([
+{
+  booking_id: 1,
+  event_name: "Concert",
+  event_date: new Date("2026-11-01"),
+  venue: "Stadium",
+  customer_name: "Asha",
+  ticket_type: "VIP",
+  number_of_tickets: 2,
+  total_price: 5000,
+  booking_date: new Date("2026-09-01"),
+  payment_status: "Paid"
+},
+{
+  booking_id: 2,
+  event_name: "Seminar",
+  event_date: new Date("2026-09-01"),
+  venue: "Hall",
+  customer_name: "Rahul",
+  ticket_type: "General",
+  number_of_tickets: 1,
+  total_price: 1000,
+  booking_date: new Date("2024-01-01"),
+  payment_status: "Pending"
+},
+{
+  booking_id: 3,
+  event_name: "Expo",
+  event_date: new Date("2026-12-01"),
+  venue: "Expo Center",
+  customer_name: "Sneha",
+  ticket_type: "Student",
+  number_of_tickets: 3,
+  total_price: 1500,
+  booking_date: new Date("2026-10-01"),
+  payment_status: "Paid"
+},
+{
+  booking_id: 4,
+  event_name: "Concert",
+  event_date: new Date("2023-01-01"),
+  venue: "Stadium",
+  customer_name: "Kiran",
+  ticket_type: "VIP",
+  number_of_tickets: 1,
+  total_price: 2500,
+  booking_date: new Date("2022-01-01"),
+  payment_status: "Paid"
+},
+{
+  booking_id: 5,
+  event_name: "Fair",
+  event_date: new Date("2026-08-01"),
+  venue: "Ground",
+  customer_name: "Meena",
+  ticket_type: "General",
+  number_of_tickets: 2,
+  total_price: 800,
+  booking_date: new Date("2023-01-01"),
+  payment_status: "Pending"
+}
+])
+db.bookings.find({
+  event_date: { $gt: new Date("2026-10-01") },
+  payment_status: "Paid"
+})
+db.bookings.updateMany(
+{
+  event_date: { $lt: new Date() },
+  payment_status: "Paid"
+},
+{ $set: { payment_status: "Refunded" } }
+)
+db.bookings.deleteMany({
+  payment_status: "Pending",
+  booking_date: { $lt: new Date("2024-01-01") }
+})
+db.bookings.aggregate([
+{
+  $group: {
+    _id: "$event_name",
+    total_tickets: { $sum: "$number_of_tickets" }
+  }
+}
+])
