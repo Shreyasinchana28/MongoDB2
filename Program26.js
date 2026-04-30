3e6296eeb1edf73d4a5c5f51933efd5927f4966d
Shreyasinchana28 <shreyasinchanar@gmail.com>
2026-04-30 11:57:01 +0530

    Create Program26.js

---
+++ Program26.js
@@ -0,0 +1,70 @@
+db.riders.insertMany([
+{
+  rider_id: 1,
+  rider_name: "Amit",
+  city: "Bangalore",
+  vehicle_type: "Bike",
+  total_deliveries: 25,
+  average_rating: 4.7,
+  current_status: "Available",
+  earnings_today: 500,
+  delivery_history: [101,102]
+},
+{
+  rider_id: 2,
+  rider_name: "Ravi",
+  city: "Chennai",
+  vehicle_type: "Scooter",
+  total_deliveries: 10,
+  average_rating: 4.2,
+  current_status: "Busy",
+  earnings_today: 300,
+  delivery_history: [103]
+},
+{
+  rider_id: 3,
+  rider_name: "Sneha",
+  city: "Hyderabad",
+  vehicle_type: "Bike",
+  total_deliveries: 0,
+  average_rating: 0,
+  current_status: "Offline",
+  earnings_today: 0,
+  delivery_history: []
+},
+{
+  rider_id: 4,
+  rider_name: "Kiran",
+  city: "Mumbai",
+  vehicle_type: "Bike",
+  total_deliveries: 30,
+  average_rating: 4.8,
+  current_status: "Available",
+  earnings_today: 700,
+  delivery_history: [104,105]
+},
+{
+  rider_id: 5,
+  rider_name: "Meena",
+  city: "Delhi",
+  vehicle_type: "Scooter",
+  total_deliveries: 22,
+  average_rating: 4.6,
+  current_status: "Available",
+  earnings_today: 400,
+  delivery_history: [106]
+}
+])
+db.riders.find({
+  current_status: "Available",
+  average_rating: { $gt: 4.5 }
+})
+db.riders.updateMany(
+{ total_deliveries: { $gt: 20 } },
+{ $inc: { earnings_today: 100 } }
+)
+db.riders.deleteMany({
+  current_status: "Offline",
+  total_deliveries: 0
+})
+db.riders.createIndex({ delivery_history: 1 })
